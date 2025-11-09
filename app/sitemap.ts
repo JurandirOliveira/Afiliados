aabe00cd18ce002e3c8dd3b7631ee82d206e9a9d
jurandir <jurandir52@gmail.com>
2025-11-09 14:31:48 -0300

    fix sitemap import path

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from "next";
-import produtos from "@/data/produtos.json";
+import produtos from "../data/produtos.json";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://apps-jo.vercel.app"; // ALTERE PARA SEU DOMÍNIO REAL
