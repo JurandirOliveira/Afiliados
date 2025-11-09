b178b7a0a4e4085b3f4549952e6f03abb1a29c2e
jurandir <jurandir52@gmail.com>
2025-11-09 13:12:44 -0300

    Testando solução B

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -35,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-600`} suppressHydrationWarning>
         {children}
+        <Script src="/forced-dark.js" strategy="afterInteractive" />
         <script dangerouslySetInnerHTML={{ __html: inlineScript }} />
       </body>
     </html>
